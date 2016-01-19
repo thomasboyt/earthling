1f0f2a952abf6e400b2ba59a7593be3583c625b7
Thomas Boyt <me@thomasboyt.com>
2016-01-19 04:54:45 -0500

    Fix prod configureStore not correctly setting middleware

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
 
 import projectReducer from './projectReducer';
 const middleware = require(__PROJECT__ + '/config/middleware').default;
 
 const createStoreWithMiddleware = compose(
-  applyMiddleware(thunkMiddleware)
+  applyMiddleware(...middleware)
 )(createStore);
 
 export default function createAppStore(data) {
